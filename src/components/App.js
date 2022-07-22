f71ca4737cb3dbd72dcf3a707cd97a88bab50209
Nicholas <nicholasjoven@gmail.com>
2022-07-22 11:59:10 -0400

    finished tasklist and task deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,18 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS) // TASKS is set as the initial value of tasks
+  
+  const removeTask = (taskText) => {
+    setTasks(tasks.filter(task => task.text !== taskText))
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={tasks} removeTask={removeTask}/> 
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from './Task'
 
-function TaskList() {
+function TaskList({ tasks, removeTask }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {
+        tasks.map((element, index) => {
+          return ( 
+            <Task key={index} text={element.text} category={element.category} removeTask={removeTask} />
+          )
+        })
+      }
     </div>
   );
 }
